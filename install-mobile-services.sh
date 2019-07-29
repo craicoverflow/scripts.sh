64e6296d76ef2dc366e9e4be9c51668cf09e7db9
Enda Phelan <ephelan@redhat.com>
2019-07-29 16:56:38 +0100

    refactor: make script idempotent

--- install-mobile-services.sh
+++ install-mobile-services.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-export APP_NAMESPACE=${APP_NAMESPACE:-mobile-console}
+# Set MDC namespace
+export NAMESPACE=${NAMESPACE:-mobile-console}
+kubectl create namespace $NAMESPACE
 
 #################
 ## Install UPS ##
@@ -9,17 +11,16 @@ export APP_NAMESPACE=${APP_NAMESPACE:-mobile-console}
 # prepare cluster
 kubectl create namespace unifiedpush
 kubectl label namespace unifiedpush monitoring-key=middleware
-kubectl create namespace $APP_NAMESPACE
-kubectl create -n unifiedpush -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/service_account.yaml
-kubectl create -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/role.yaml
-kubectl create -n unifiedpush -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/role_binding.yaml
+kubectl apply -n unifiedpush -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/service_account.yaml
+kubectl apply -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/role.yaml
+kubectl apply -n unifiedpush -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/role_binding.yaml
 kubectl apply -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/crds/push_v1alpha1_unifiedpushserver_crd.yaml
 kubectl apply -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/crds/push_v1alpha1_pushapplication_crd.yaml
 kubectl apply -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/crds/push_v1alpha1_androidvariant_crd.yaml
 kubectl apply -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/crds/push_v1alpha1_iosvariant_crd.yaml
 
 # Deploy UPS operator
-cat <<EOF | kubectl apply -n $NAMESPACE -f -
+cat <<EOF | kubectl apply -n unifiedpush -f -
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -45,7 +46,7 @@ spec:
             - name: WATCH_NAMESPACE
               value: ""
             - name: APP_NAMESPACES
-              value: $APP_NAMESPACE
+              value: $NAMESPACE
             - name: SERVICE_NAMESPACE
               valueFrom:
                 fieldRef:
@@ -59,26 +60,26 @@ spec:
 EOF
 
 # install UPS application
-kubectl apply -n $NAMESPACE -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/crds/push_v1alpha1_unifiedpushserver_cr.yaml
+kubectl apply -n unifiedpush -f https://raw.githubusercontent.com/aerogear/unifiedpush-operator/master/deploy/crds/push_v1alpha1_unifiedpushserver_cr.yaml
 
 
 #####################################
 ## Install Mobile Security Service ##
 #####################################
 
-kubectl create namespace $mobile-security-service
-kubectl label namespace $mobile-security-service monitoring-key=middleware
-kubectl create namespace $APP_NAMESPACE
-kubectl create -f https://raw.githubusercontent.com/aerogear/mobile-security-service-operator/master/deploy/cluster_role.yaml
-kubectl create -f https://raw.githubusercontent.com/aerogear/mobile-security-service-operator/master/deploy/cluster_role_binding.yaml
-kubectl create -f https://raw.githubusercontent.com/aerogear/mobile-security-service-operator/master/deploy/service_account.yaml
+oc new-project mobile-security-service
+kubectl label namespace mobile-security-service monitoring-key=middleware
+kubectl create namespace $NAMESPACE
+kubectl apply -n mobile-security-service -f https://raw.githubusercontent.com/aerogear/mobile-security-service-operator/master/deploy/cluster_role.yaml
+kubectl apply -n mobile-security-service -f https://raw.githubusercontent.com/aerogear/mobile-security-service-operator/master/deploy/cluster_role_binding.yaml
+kubectl apply -n mobile-security-service -f https://raw.githubusercontent.com/aerogear/mobile-security-service-operator/master/deploy/service_account.yaml
 kubectl apply -f https://raw.githubusercontent.com/aerogear/mobile-security-service-operator/master/deploy/crds/mobile-security-service_v1alpha1_mobilesecurityservice_crd.yaml
 kubectl apply -f https://raw.githubusercontent.com/aerogear/mobile-security-service-operator/master/deploy/crds/mobile-security-service_v1alpha1_mobilesecurityservicedb_crd.yaml
 kubectl apply -f https://raw.githubusercontent.com/aerogear/mobile-security-service-operator/master/deploy/crds/mobile-security-service_v1alpha1_mobilesecurityserviceapp_crd.yaml
 kubectl apply -f https://raw.githubusercontent.com/aerogear/mobile-security-service-operator/master/deploy/crds/mobile-security-service_v1alpha1_mobilesecurityservicebackup_crd.yaml
 
 # Deploy Mobile Security Service operator
-cat <<EOF | kubectl apply -n $mobile-security-service -f -
+cat <<EOF | kubectl apply -n mobile-security-service -f -
 apiVersion: apps/v1
 kind: Deployment
 metadata:
@@ -107,7 +108,7 @@ spec:
                 fieldRef:
                   fieldPath: metadata.namespace
             - name: APP_NAMESPACES
-              value: $APP_NAMESPACE
+              value: $NAMESPACE
             - name: POD_NAME
               valueFrom:
                 fieldRef:
